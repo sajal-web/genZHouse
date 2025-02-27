bb837969a299ab429a33c0f1a195d59af5b51ca7
sajal.mahata <sajal.mahata@sentientgeeks.com>
2025-02-27 19:00:28 +0530

    * Added welcome activity. * Add the design of welcome activity. * Added splash scree.

--- app/src/main/java/com/example/genzhouse/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/genzhouse/ui/welcome/WelcomeActivity.kt
@@ -1,4 +1,4 @@
-package com.example.genzhouse
+package com.example.genzhouse.ui.welcome
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -6,18 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.genzhouse.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                runBlocking {
+                    delay(1000)
+                    false
+                }
+            }
         }
+        setContentView(R.layout.activity_welcome)
     }
 }
